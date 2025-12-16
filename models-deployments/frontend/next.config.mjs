d888a8aec8f9ca433ca84ceb5c6a652a52356bc4
Achinta Hazra <achintahazra8515@gmail.com>
2025-12-16 12:12:13 +0530

    d

--- models-deployments/frontend/next.config.mjs
+++ models-deployments/frontend/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   eslint: {
     ignoreDuringBuilds: true
+  },
+  experimental: {
+    serverActions: false,
+    ppr: false
   }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
